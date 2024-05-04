4fe64e0fba263df7744f25cfeac7c0543f5d1bd3
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-05-04 21:26:26 +0100

    Starting out my java coding

--- hello.java
+++ hello.java
@@ -1,8 +1,16 @@
 
 class HelloWorld{
-    public static void main(String arg[]){
-        char marach1 = 'y';
-        marach1++;
-        System.out.print(marach1);
+    public static void main(String[] arg){
+        int numbers [] = new int[10];
+        // char marach1 = 'x';
+        // char david = marach1++;
+        // System.out.println(marach1);
+        // System.out.print(david);
+
+        //for(int marach: numbers) System.out.println(marach);
+
+        StringBuffer marach = new StringBuffer("God is very good to me!");
+        marach.append("...Because he chase me from 9th mile");
+        System.out.println(marach);
     }
 }
