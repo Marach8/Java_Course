929913ed1fdec53d46774daab4acb435d3fc1508
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-05-17 12:13:02 +0100

    Learned about classes and manipulation of its method

--- Hello.java
+++ Hello.java
@@ -4,5 +4,21 @@ public class Hello {
     public static void main(String [] args){
         System.out.println("This is my first time using intelliJ");
         System.out.println("I actually love writing Java");
+        Laptop hp = new Laptop();
+        hp.printNameOfLaptop();
+
+    }
+}
+
+
+class Computer{
+    public void printNameOfLaptop(){
+        System.out.println("This Laptop is now giving me joy");
+    }
+}
+class Laptop extends Computer{
+    @Override
+    public void printNameOfLaptop(){
+        System.out.println("IntelliJ IDEA is a nice editor for Java");
     }
 }
