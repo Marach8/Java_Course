3c2d26ec859cdf5f27bb7e8f40708d9e88094cfc
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-05-17 01:27:49 -0700

    Started writing java on intelliJ idea

---
+++ Hello.java
@@ -0,0 +1,8 @@
+package Java_Course;
+
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("This is my first time using intelliJ");
+        System.out.println("I actually love writing Java");
+    }
+}
