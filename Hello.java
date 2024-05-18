5feb77d76d6340d70ba1623ffb3678866f09ea0c
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-05-18 09:19:44 +0100

    Learned about concise or for each loop in java

--- Hello.java
+++ Hello.java
@@ -5,8 +5,12 @@ public class Hello {
         System.out.println("This is my first time using intelliJ");
         System.out.println("I actually love writing Java");
         Laptop hp = new Laptop();
+        Laptop.Nnanna myName = hp.new Nnanna();
         hp.printNameOfLaptop();
-
+        Laptop.NamesOfPeople[] namesOfPeople = Laptop.NamesOfPeople.values();
+        for(var name : namesOfPeople){
+            System.out.println(name);
+        }
     }
 }
 
@@ -20,5 +24,14 @@ class Laptop extends Computer{
     @Override
     public void printNameOfLaptop(){
         System.out.println("IntelliJ IDEA is a nice editor for Java");
+    }
+
+    public enum NamesOfPeople {Daniel, Emmanuel, David, Okoro;}
+
+    class Nnanna{
+        final String name = "Ajah Emmanuel Nnanna";
+        public void printNameOfPerson(){
+            System.out.println(name);
+        }
     }
 }
