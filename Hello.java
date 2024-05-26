92005d2da418ddf6ee622198bd6b2691824e857f
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-05-26 20:54:34 +0100

    Learned how to take input from the user

--- Hello.java
+++ Hello.java
@@ -1,37 +1,15 @@
 package Java_Course;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Hello {
-    public static void main(String [] args){
-        System.out.println("This is my first time using intelliJ");
-        System.out.println("I actually love writing Java");
-        Laptop hp = new Laptop();
-        Laptop.Nnanna myName = hp.new Nnanna();
-        hp.printNameOfLaptop();
-        Laptop.NamesOfPeople[] namesOfPeople = Laptop.NamesOfPeople.values();
-        for(var name : namesOfPeople){
-            System.out.println(name);
-        }
-    }
-}
+    public static void main(String [] args) throws IOException {
 
-
-class Computer{
-    public void printNameOfLaptop(){
-        System.out.println("This Laptop is now giving me joy");
-    }
-}
-class Laptop extends Computer{
-    @Override
-    public void printNameOfLaptop(){
-        System.out.println("IntelliJ IDEA is a nice editor for Java");
-    }
-
-    public enum NamesOfPeople {Daniel, Emmanuel, David, Okoro;}
-
-    class Nnanna{
-        final String name = "Ajah Emmanuel Nnanna";
-        public void printNameOfPerson(){
-            System.out.println(name);
-        }
+        final InputStreamReader reader = new InputStreamReader(System.in);
+        final BufferedReader bufferedReader = new BufferedReader(reader);
+        final var name = bufferedReader.readLine();
+        String fullName = "Your name is " + name;
+        System.out.println(fullName);
     }
 }
