6c06e3c58fd1fce4221639ec3a624bd2ad40df3d
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-05-27 15:23:58 +0100

    Learned about Maps in Java

--- Hello.java
+++ Hello.java
@@ -1,29 +1,17 @@
 package Java_Course;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Hello {
-    public static void main(String [] args) {
-        Runnable runnable1 = () -> {
-            for(int i = 0; i < 5; i++){
-                System.out.println("Marach");
-                try{
-                    Thread.sleep(10);
-                } catch(Exception e){}
-            }
-        };
+    public static void main(String [] args){
+        Collection<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(9);
+        //System.out.print(numbers);
 
-        Runnable runnable2 = () -> {
-            for(int i = 0; i < 5; i++) {
-                System.out.println("Emmanuel");
-                try{
-                    Thread.sleep(10);
-                } catch(Exception e){}
-            }
-        };
-
-        Thread thread1 = new Thread(runnable1);
-        Thread thread2 = new Thread(runnable2);
-
-        thread1.start();
-        thread2.start();
+        Map<String, String> map = new HashMap<>();
+        map.putIfAbsent("name", "Ajah Emmmanue Amarachi");
+        System.out.println(map);
     }
 }
